7c3422d4b1be4276aaf10933a79038a64907a9e6
Anush Shetty <anush@gluster.com>
2011-05-23 16:06:43 +0530

    config changes

--- config.py
+++ config.py
@@ -7,28 +7,30 @@ import time
 import datetime
 
 START_TIMESTR = time.strftime("%d-%b-%Y-%H-%M-%S", time.localtime())
-BRICKS_IPADDRS=[]
-SERVER_EXPORT='/mnt/sanity'
+BRICKS_IPADDRS=['192.168.1.55']
+SERVER_EXPORT='/mnt/'
 TRANSPORT='tcp'
-NUM_BRICKS='1'
-REPLICA= True
+NUM_BRICKS=1
+REPLICA= False
 RUNTESTS= True
-SETUPBRICKS = True
+SETUPBRICKS = False
 VOL_PAR=""
 NFSSERVER_ADDR=''
 FUSE=True
 TESTVOLUME=['testvol']
 
-CLIENT_IP=''
+CLIENT_IP='192.168.1.55'
 MOUNTPOINT = "/tmp/sanity-testsdir-"+START_TIMESTR+"/"
 
 LOGFILE = "/tmp/testbot-log-"
 LOGSCPURL = ""
 LOGDOWNLOADURL = "http://shell.gluster.com/~anush/testbot"
+
 MAILUSER = "anush@gluster.com"
 MAILPWD = ""
 MAILSRV = "localhost"
-MAILTO = ""
+MAILRECIEVER = "anushshetty@gmail.com"
+
 TESTVOLUMES = []
 START = time.time()
 START_TIMESTR = time.strftime("%d-%b-%Y-%H-%M-%S", time.localtime())
@@ -40,4 +42,4 @@ TESTBOT_DOWNLOAD_DIR = "/tmp/shehjart-testbot-"+START_TIMESTR+"/"
 DAEMONIZE = True
 TESTTYPE = ""
 TESTNAMES = ["dbench"]
-EMAILCONTROLLOG = False
+EMAILCONTROLLOG = True
